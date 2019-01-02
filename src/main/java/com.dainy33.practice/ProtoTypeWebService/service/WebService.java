bc91e4123a6430500075912a6c2935986d5f05a9
sx-9531 <sx-wangbn@dtdream.com>
2019-01-02 10:08:52 +0800

    spring-cxf

--- src/main/java/com.dainy33.practice/ProtoTypeWebService/service/WebService.java
+++
@@ -1,9 +0,0 @@
-package com.dainy33.practice.ProtoTypeWebService.service;
-
-import javax.jws.WebMethod;
-
-@javax.jws.WebService
-public interface WebService {
-    @WebMethod
-    public String sayHello();
-}
